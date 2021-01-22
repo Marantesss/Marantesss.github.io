3ec3bcbe8378ba889c098b7a137cc8fa957d9508
Marantesss <up201706917@fe.up.pt>
2021-01-22 12:02:37 +0000

    :sparkles: animated header

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="relative h-screen grid grid-cols-5 circle-clip header-min-height">
+  <div
+    ref="headerContainer"
+    class="relative h-screen grid grid-cols-5 circle-clip header-min-height overflow-hidden"
+  >
     <!-- background -->
     <div class="md:col-span-2 md:inline hidden h-full bg-dark-blue"></div>
     <div class="md:col-span-3 col-span-5 h-full bg-light-blue"></div>
@@ -7,14 +10,14 @@
     <div class="absolute h-full w-full">
       <div class="h-full flex">
         <!-- photo -->
-        <div class="md:w-1/2 md:flex hidden justify-center">
+        <div ref="heroPhoto" class="md:w-1/2 md:flex hidden justify-center">
           <img src="~assets/img/test.png" />
         </div>
         <!-- title -->
         <div
           class="h-full flex items-center justify-center md:justify-start w-full md:w-1/2"
         >
-          <div class="hero-card md:p-0 p-8">
+          <div ref="heroCard" class="hero-card md:p-0 p-8">
             <h1 class="text-gray-700 text-left font-title hero-title">
               Gonçalo
             </h1>
@@ -55,10 +58,54 @@ export default {
       },
     ],
   }),
+
+  mounted() {
+    const heroCard = this.$refs.heroCard
+    const heroPhoto = this.$refs.heroPhoto
+    const headerContainer = this.$refs.headerContainer
+
+    // create timeline
+    const timeline = this.$gsap.timeline()
+
+    timeline
+      // curve bottom header
+      .fromTo(
+        headerContainer,
+        { clipPath: 'none' },
+        {
+          clipPath: 'circle(300vh at 50% -200vh)',
+          duration: 1,
+          ease: 'power2.out',
+        }
+      )
+      // slide from right to left
+      .fromTo(
+        heroCard,
+        { opacity: 0, x: '200%' },
+        { opacity: 1, x: '0%', duration: 1, ease: 'power2.out' }
+      )
+      // slide from left to right
+      .fromTo(
+        heroPhoto,
+        { opacity: 0, x: '-200%' },
+        { opacity: 1, x: '0%', duration: 1, ease: 'power2.out' },
+        '-=1'
+      )
+  },
 }
 </script>
 
 <style scoped>
+@media only screen and (max-height: 500px) {
+  .circle-clip {
+    clip-path: none !important;
+  }
+}
+
+.circle-clip {
+  clip-path: circle(300vh at 50% -200vh);
+}
+
 .header-min-height {
   min-height: 500px;
 }
