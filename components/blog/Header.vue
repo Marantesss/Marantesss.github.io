676262b55c4bf18d2987b57624e8dfdb94fb6892
Marantesss <up201706917@fe.up.pt>
2021-01-24 00:31:18 +0000

    :construction: working on blog header

---
+++ components/blog/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="bg-dark-blue header-min-height">
+    <div class="text-white text-4xl flex justify-center h-full">
+      WIP
+      <dynamic-image class="h-64" filename="workspace.svg"></dynamic-image>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data: () => ({
+    navbarItems: [
+      {
+        name: 'Home',
+        route: '/',
+      },
+      {
+        name: 'Blog',
+        route: '/blog',
+      },
+    ],
+  }),
+
+  mounted() {
+    /*
+    const heroCard = this.$refs.heroCard
+    const heroPhoto = this.$refs.heroPhoto
+    const headerContainer = this.$refs.headerContainer
+
+    // create timeline
+    const timeline = this.$gsap.timeline()
+
+    timeline
+      // curve header
+      .fromTo(
+        headerContainer,
+        { clipPath: 'none' },
+        {
+          clipPath: 'circle(300vh at 50% -200vh)',
+          duration: 2,
+          ease: 'power2.inOut',
+        }
+      )
+      // slide from right to left
+      .fromTo(
+        heroCard,
+        { opacity: 0, x: '200%' },
+        { opacity: 1, x: '0%', duration: 1, ease: 'power2.out' },
+        '-=1'
+      )
+      // slide from left to right
+      .fromTo(
+        heroPhoto,
+        { opacity: 0, x: '-200%' },
+        { opacity: 1, x: '0%', duration: 1, ease: 'power2.out' },
+        '-=1'
+      )
+      */
+  },
+}
+</script>
+
+<style scoped>
+@media only screen and (max-height: 500px) {
+  .circle-clip {
+    clip-path: none !important;
+  }
+}
+
+.circle-clip {
+  clip-path: circle(300vh at 50% -200vh);
+}
+
+.header-min-height {
+  min-height: 500px;
+}
+
+.hero-card {
+  width: 40rem;
+}
+
+.hero-title {
+  font-size: 6rem;
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="container mx-auto">
-    <section class="grid grid-cols-1 gap-4">
-      <div v-for="post in posts" :key="post.title">
-        <PostCard
-          :image="post.image"
-          :title="post.title"
-          :description="post.description"
-          :author="post.author"
-          :date="post.date"
-          :url="post.slug"
-          :tags="post.tags"
-          :time="post.time"
-        />
-      </div>
-    </section>
+  <div>
+    <Header />
+    <!-- Content -->
+    <div class="container mx-auto">
+      <section class="my-32 grid grid-cols-1 gap-4">
+        <div v-for="post in posts" :key="post.title">
+          <PostCard
+            :image="post.image"
+            :title="post.title"
+            :description="post.description"
+            :author="post.author"
+            :date="post.date"
+            :url="post.slug"
+            :tags="post.tags"
+            :time="post.time"
+          />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 import PostCard from '~/components/blog/PostCard'
+import Header from '~/components/blog/Header'
 
 export default {
   name: 'Blog',
 
   components: {
     PostCard,
+    Header,
   },
 
-  layout: 'blog',
+  layout: 'default',
 
   async asyncData({ $content }) {
     const posts = await $content('articles').fetch()
