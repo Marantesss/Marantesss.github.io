283f1e5140ad3e1386ee202a52617517fa65e624
Marantesss <up201706917@fe.up.pt>
2021-01-25 10:37:45 +0000

    :bug: fix 404 on dynamic content routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,16 @@ export default {
     '@nuxt/content',
   ],
 
+  generate: {
+    // https://content.nuxtjs.org/advanced/#static-site-generation
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    },
+  },
+
   // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {},
 
