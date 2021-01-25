f2ab896e879ac64168abae3e0b5499567da9fa72
Marantesss <up201706917@fe.up.pt>
2021-01-25 17:05:56 +0000

    :mag: add global meta tags and twiiter + open graph tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,87 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Marantesss 👋',
+    title: 'Marantesss',
+    // favicon
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // meta tags
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        // to avoid any duplication when used in child component, please give a unique identifier with the hid key
+        hid: 'description',
+        name: 'description',
+        content:
+          'Hello, I am Gonçalo Marantes, a Software Engineering student from Portugal. And this is my personal website and blog about anything mildly interesting.',
+      },
+
+      // Twitter
+      // Test on: https://cards-dev.twitter.com/validator
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@marantesss' },
+      {
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: process.env.BASE_URL,
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Marantesss',
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'Hello, I am Gonçalo Marantes, a Software Engineering student from Portugal. And this is my personal website and blog about anything mildly interesting.',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: `${process.env.BASE_URL}/social-card.png`,
+      },
+
+      // Open Graph
+      // Test on: https://developers.facebook.com/tools/debug/
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Marantesss' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://nuxtjs.org',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Marantesss',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Hello, I am Gonçalo Marantes, a Software Engineering student from Portugal. And this is my personal website and blog about anything mildly interesting.',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `${process.env.BASE_URL}/social-card.png`,
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: `${process.env.BASE_URL}/social-card.png`,
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Marantesss',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
