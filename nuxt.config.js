1ea885b37c7d908ab70c4580460daba59187644b
Marantesss <up201706917@fe.up.pt>
2021-01-26 15:13:35 +0000

    :bug: forgot to remove unused plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -124,7 +124,7 @@ export default {
   css: ['~assets/css/main'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['plugins/preview.client.js'],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
